bc7922e5ab4c4cdb7eed00fea30edaac08347ced
Kamz <kaumilmanzoor@gmail.com>
2020-11-01 17:26:18 -0500

    Overlapping heashots with z index

--- src/components/card/players.js
+++ src/components/card/players.js
@@ -21,13 +21,13 @@ const Players = () => {
   }, []);
 
   return (
-    <ul className={styles.navigation}>
+    <ul className={styles.container}>
       {data.players.map((player, i) => (
-        <li key={i} className={styles.box}>
-          <div>
+        <li key={i} style={{ zIndex: 10 - i }} className={styles.box}>
+          <div className={styles.imageBackground}>
             <img className={styles.pic} src={hashTable.get(player.last_name)} />
           </div>
-          <div>{player.last_name}</div>
+          <div className={styles.playerName}>{player.last_name}</div>
           <div className={styles.points}>{player.points}</div>
           <div className={styles.pointsText}>pts</div>
         </li>
