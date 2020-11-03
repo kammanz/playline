8a48b5b71f63325d575994b5c269339051186dfd
Kamz <kaumilmanzoor@gmail.com>
2020-11-02 23:39:53 -0500

    Added border shadow to points container

--- src/apis/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const axiosOne = async () => {
-  return await axios.get(
-    'https://playline-dev-test.s3-us-west-2.amazonaws.com/playline-test.json'
-  );
-};
-
-export default axiosOne;
--- src/components/card/players.js
+++ src/components/card/players.js
@@ -17,19 +17,23 @@ const Players = () => {
     fetchData();
   }, []);
 
+  const playersLength = data.players.length;
+
   return (
     <ul className={styles.container}>
       {data.players.map((player, i) => (
         <li
           key={i}
-          style={{ zIndex: data.players.length - i }}
+          style={{ zIndex: playersLength - i }}
           className={styles.box}>
           <div className={styles.imageBackground}>
             <img className={styles.pic} src={hashTable.get(player.last_name)} />
           </div>
           <div className={styles.playerName}>{player.last_name}</div>
-          <div className={styles.points}>{player.points}</div>
-          <div className={styles.pointsText}>pts</div>
+          <div className={styles.pointsContainer}>
+            <div className={styles.points}>{player.points}</div>
+          </div>
+          <div className={styles.pointsText}>Pts</div>
         </li>
       ))}
     </ul>
